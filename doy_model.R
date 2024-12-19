7d3e4284bb95484893aa9345dbfc07a1a3090a0f
Thomas Ohnemus <thomas.ohnemus@ufz.de>
2024-12-19 11:47:07 +0100

    doy: added rmse calculation

--- doy_model.R
+++ doy_model.R
@@ -8,25 +8,46 @@ rm(list = ls())
 library(tidyverse)
 library(data.table)
 
+# Functions
+rmse <- function(actual, predicted){
+  sqrt(mean((actual - predicted)^2))
+}
+
 # Load Data
 setwd("C:/Docs/MIRO/vegetation_model")
 
 # Phenology Observations
 obs    <- read.csv("Phenology_Observations.csv")
+obs    <- obs[(obs$Phase_id == 3 |
+                obs$Phase_id == 5 |
+                obs$Phase_id == 6) &
+                obs$Referenzjahr > 1995,]
 
 # Frequency table
-freq    <- data.frame(table(obs$SORTE))
+freq    <- obs %>% group_by(SORTE, Phase_id) %>%
+  summarize(n = n())
 
 # Filter only cultivars with less than 60 observations (i.e. less than 2 observations per year)
-freq    <- freq[freq$Freq <= 60,]
+freq    <- freq[freq$n <= 60,]
+unindentified <- unique(freq$SORTE)
 
 # Shift these to the "unidentified" category
-obs$SORTE[obs$SORTE %in% freq$Var1] <- "unidentified"
+obs$SORTE[obs$SORTE %in% unindentified] <- "unidentified"
 
-# Cultivar-specific analyses
-# 1. Variation Bud Break Date within Cultivar
-# Calculate mean DOY for each cultivar
+
+## Subset in calibration & validation dataset
+
+
+
+# Calculate mean DOY for each cultivar & phase
 doy  <- obs %>% 
-  group_by(SORTE) %>%
+  group_by(SORTE, Phase_id) %>%
   summarize(mean_doy = mean(Jultag),
             sd = sd(Jultag))
+
+# Calculate rmse per variety & phase
+obs     <- left_join(obs, doy, by = c("SORTE", "Phase_id"))
+rmse_df <- obs %>% group_by(SORTE, Phase_id) %>%
+  summarize(rmse = rmse(Jultag, mean_doy))
+
+
