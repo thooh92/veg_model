a4364b41634aa4426dbce39b7b67bd9e4960f497
Thomas Ohnemus <thomas.ohnemus@ufz.de>
2025-01-06 11:31:23 +0100

    Initiated ChillOverlap.R

---
+++ ChillOverlap.R
@@ -0,0 +1,32 @@
+## Author: Thomas Ohnemus
+## Date: 06/01/2025
+## Chill Overlap Model
+
+# Prepare Script
+rm(list = ls())
+
+library(tidyverse)
+library(data.table)
+library(chillR)
+
+# Observations
+
+
+# Variety infos (including min CP observed before bud break)
+
+
+# Estimate b2 for each phenological stage
+b2 <- tst # highest HA - lowest HA per stage & cultivar
+
+
+
+# Initiate CP vector for cultivar
+CPs <- seq(30, max(), 1)
+
+
+# Estimate b1 for each phenological stage
+b1 <- tst # Calculate min HR found between day of CR fulfillment and phenological stage investigated
+
+
+
+
